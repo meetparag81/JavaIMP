a2364ecc455ce5a65f1cd10c3e70aa4c48f61e05
meetparag81 <paragborawake81@gmail.com>
2019-09-14 21:46:20 +0530

    frame changes

--- JavaIMP/src/main/java/IFrame/FramesTest.java
+++ JavaIMP/src/main/java/IFrame/FramesTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import Multiplewindows.ResourceHelper;
+import Helpers.ResourceHelper;
 
 public class FramesTest 
 {
