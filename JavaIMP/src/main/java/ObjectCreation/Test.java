bb81e1cee2c7b4d3c0bdb1f338a0caf2201b360f
parag.borawake <paragborawake81@gmail.com>
2020-02-27 17:53:40 +0530

    Object creation

---
+++ JavaIMP/src/main/java/ObjectCreation/Test.java
@@ -0,0 +1,14 @@
+package ObjectCreation;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		A obj = new A();
+		B bbj1= new B(0);
+		B obj2= new B(0);
+		
+		obj2.Test();
+		obj.Test();
+	}
+
+}
