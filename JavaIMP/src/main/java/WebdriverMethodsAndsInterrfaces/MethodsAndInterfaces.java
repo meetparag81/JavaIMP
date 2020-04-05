09192a70b4a9b9e53750a86e91207992e7f03571
meetparag81 <paragborawake81@gmail.com>
2020-04-05 22:34:39 +0530

    webdriver package created with diffrent classes

---
+++ JavaIMP/src/main/java/WebdriverMethodsAndsInterrfaces/MethodsAndInterfaces.java
@@ -0,0 +1,7 @@
+package WebdriverMethodsAndsInterrfaces;
+
+public class MethodsAndInterfaces 
+{
+	System.setProperty("webdriver.chrome.driver",ResourceHelper.GetResourcePath("\\src\\main\\resorces\\driver\\chromedriver.exe"));
+	
+}
