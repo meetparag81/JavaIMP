cc69b5de489fff385dc038a26a6cc7de9b145a4d
meetparag81 <paragborawake81@gmail.com>
2020-04-09 22:06:38 +0530

    method and interfaces class added in java imp prj

--- JavaIMP/src/main/java/WebdriverMethodsAndsInterrfaces/MethodsAndInterfaces.java
+++ JavaIMP/src/main/java/WebdriverMethodsAndsInterrfaces/MethodsAndInterfaces.java
@@ -1,7 +1,28 @@
 package WebdriverMethodsAndsInterrfaces;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Options;
+import org.openqa.selenium.WebDriver.Window;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import Multiplewindows.ResourceHelper;
+
 public class MethodsAndInterfaces 
 {
-	System.setProperty("webdriver.chrome.driver",ResourceHelper.GetResourcePath("\\src\\main\\resorces\\driver\\chromedriver.exe"));
+	 
+	private void main() {
+		System.setProperty("webdriver.chrome.driver" ,ResourceHelper.getResourcePath("\\src\\main\\resources\\chromedriver.exe"));
+		WebDriver driver = new ChromeDriver();
+		//driver. manage returns the options intrface.
+		 Options var = driver.manage();
+		//driver. manage. window returns the windows intrface.
+		Window var1 = var.window();
+		var1.maximize();
+		//driver. manage(). window().maximize() method returns  the maximized window.
+		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		 
+	}
 	
 }
