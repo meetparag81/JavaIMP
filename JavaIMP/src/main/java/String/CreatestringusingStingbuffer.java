bdeee5afbe84d3a30df9d7928d88ad8436cb294d
parag.borawake <paragborawake81@gmail.com>
2020-04-24 17:00:56 +0530

    mod and checksum no code added in java imp

--- JavaIMP/src/main/java/String/CreatestringusingStingbuffer.java
+++ JavaIMP/src/main/java/String/CreatestringusingStingbuffer.java
@@ -1,8 +1,11 @@
 package String;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreatestringusingStingbuffer {
 	private static String Firstname="John" ;
@@ -11,6 +14,7 @@ public class CreatestringusingStingbuffer {
 	private static String lastName="Peter";	
 	private static String DOB;
 	private static StringBuilder sb;
+				private static Map alphabets;
 	
 
 	public static void main(String[] args) {
@@ -110,6 +114,41 @@ public class CreatestringusingStingbuffer {
 				sb.append(NumericString.charAt(index));
 			}
 			System.out.println("characters are" + sb + " and no of characters" + sb.length());
+			
+			String words = new String("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		       ArrayList<Character> CharList = new ArrayList<Character>();
+
+		       for(int i = 0; i<words.length(); i++){
+		    	   CharList.add(words.charAt(i));
+		         }
+		       int[] arr = new int[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26};
+
+		       ArrayList<Character> Nuberlist = new ArrayList<Character>();
+
+		       for(int i = 0; i<arr.length; i++){
+		    	   
+		         }
+		       
+  
+		       for(int j=0;j<26;j++)
+		       {
+		        alphabets = new HashMap<String, Integer>();
+				alphabets.put(CharList.get(j), j);
+				
+				System.out.println("The  character and numbers are"+ CharList.get(j)+","+ j );
+		       }
+		       
+		       int sum = 0;
+		       
+		       
+		       ArrayList<Character> CharList1 = new ArrayList<Character>();
+		/*
+		 * for(int k=0;k<26;k++) { String NID= sb.toString(); char s = NID.charAt(k);
+		 * alphabets.get(k);
+		 * 
+		 * 
+		 * }
+		 */
 			
 			
 		
---
+++ JavaIMP/src/main/java/StringBuffer/ModCalculationUsingString.java
@@ -0,0 +1,33 @@
+package StringBuffer;
+
+public class ModCalculationUsingString {
+	
+	public static void main(String[] args) {
+		
+		String NumericString = "0123456789";
+		StringBuilder sb = new StringBuilder(9);
+		for (int i = 0; i < 9; i++) {
+			int index = (int) (NumericString.length() * Math.random());
+			sb.append(NumericString.charAt(index));
+		}
+		int checkvalue = (int) (Double.valueOf(sb.toString()) % 23);
+		String k = String.valueOf(checkvalue);
+		
+		String words = new String("TRWAGMYFPDXBNJZSQVHLCKE");
+		int count=1;
+		for(int i=0;i<words.length();i++)
+		{
+			
+			if (i == checkvalue) {
+				sb.append(words.charAt(i));
+				break;
+				
+			}
+			
+		}
+		System.out.println("characters are" + sb + " and no of characters" + sb.length());
+		
+	
+
+}
+}
