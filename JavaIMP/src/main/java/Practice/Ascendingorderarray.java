5b63276c5a0bece7315c6f18759b9e33faabe373
Parag Borawake <v-pborawake@microsoft.com>
2021-11-15 11:30:08 +0530

    Array Program-15-11-21

---
+++ JavaIMP/src/main/java/Practice/Ascendingorderarray.java
@@ -0,0 +1,23 @@
+package Practice;
+//
+public class Ascendingorderarray {
+	public static void main(String[] args) {
+		  int [] arr = new int [] {5, 2, 8, 7, 1}; 
+			int temp=0;
+			
+			
+			for(int i=0;i<arr.length;i++){
+				for(int j=i+1;j<arr.length;j++)
+					 if(arr[i] > arr[j]) {    
+		                   temp = arr[i];    
+		                   arr[i] = arr[j];    
+		                   arr[j] = temp;    
+		               }    
+				
+			}
+			for(int k=0;k<arr.length;k++)
+			System.out.println("Ascending array is"+arr[k]);
+		
+	}
+	
+}
---
+++ JavaIMP/src/main/java/Practice/SecondhighestorhighestNoInArray.java
@@ -0,0 +1,28 @@
+package Practice;
+
+public class SecondhighestorhighestNoInArray {
+
+	public static void main(String[] args) {
+		int temp=0;
+		
+	int[] arr= new int[]{-1,0,1,60,80,99,999};
+	int  total=arr.length;
+	//highestno in array
+	for(int i=0;i<arr.length;i++){
+		for(int j=i+1;j<arr.length;j++){
+		if(arr[i]>arr[j]){
+			temp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+			
+		}
+		
+		}
+	}
+	System.out.println("second largest no in array is "+arr[total-2]);
+	System.out.println("Largest no in array is "+arr[total-1]);
+	System.out.println("smallest no in array is "+arr[0]);
+	System.out.println("secondsmallest no in array is "+arr[1]);
+	}
+
+}
